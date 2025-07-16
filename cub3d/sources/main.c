65064e76d053677dba88cbffe9b90846a97ed68e
El Mehdi El Garouaz <eel-garo@e3r4p13.1337.ma>
2025-07-16 12:30:23 +0100

    Add 3d rendering

--- cub3d/sources/main.c
+++ cub3d/sources/main.c
@@ -6,103 +6,194 @@
 /*   By: eel-garo <eel-garo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 20:23:58 by ymazini           #+#    #+#             */
-/*   Updated: 2025/07/15 16:27:27 by eel-garo         ###   ########.fr       */
+/*   Updated: 2025/07/16 11:40:58 by eel-garo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
- void	parse_texture(char **tokens, t_game *data)
+void	parse_texture(char **tokens, t_game *data)
 {
-	char *id;
-	char *path;
-	int fd;
+	char	*id;
+	char	*path;
+	int		fd;
 
 	id = tokens[0];
 	path = tokens[1];
-
-	fd = open(path,O_RDONLY);
+	fd = open(path, O_RDONLY);
 	if (fd < 0)
+		exit_with_error("fd prln", data);
+	// printf("%s\n%i\n\n\n\n",path,fd);
+
 		// TODO: Exit WITH ERROR
 	close(fd);
-		
-	// NO SO WE EA
-	if (ft_strncmp("NO",id,3) == 0)
+	if (ft_strncmp("NO", id, 3) == 0)
 	{
 		if (data->checklist.no == 1)
 			exit_with_error("duplication of Identifier in map ",data);
-	
 		data->checklist.no = 1;
 		data->asset_data.north_tex_path = ft_strdup(path);
 		// if (!data->asset_data.north_tex_path)
 			// here free the prev and then call exit_with_error and return && return (NULL);
-	
 	}
-	if (ft_strncmp("SO",id,3) == 0)
+	if (ft_strncmp("SO", id, 3) == 0)
 	{
 		if (data->checklist.so == 1)
-			exit_with_error("duplication of Identifier in map ",data);
+			exit_with_error("duplication of Identifier in map ", data);
 		data->checklist.so = 1;
 		data->asset_data.south_tex_path = ft_strdup(path);
 		// TODO: PROTET the dup if faild malloc			
-		
 	}
-	if (ft_strncmp("WE",id,3) == 0)
+	if (ft_strncmp("WE", id, 3) == 0)
 	{
 		if (data->checklist.we == 1)
-			exit_with_error("duplication of Identifier in map ",data);
+			exit_with_error("duplication of Identifier in map", data);
 		data->checklist.we = 1;
 		data->asset_data.west_tex_path = ft_strdup(path);
 		// TODO: protect her 
 	}
-	
-	if (ft_strncmp("EA",id,3) == 0)
+	if (ft_strncmp("EA", id, 3) == 0)
 	{
 		if (data->checklist.ea == 1)
-			exit_with_error("duplication of Identifier in map ",data);
-		
+			exit_with_error("duplication of Identifier in map ", data);
 		data->checklist.ea = 1;
 		data->asset_data.east_tex_path = ft_strdup(path);
 		// TODO: protect her 
 	}
-	
-
 }
 
-// void	validate_map_content(t_game *data)
-// {
-// 	int x;
-// 	int y;
-// 	char c;
-	
-// 	data->map.player_count = 0;
-// 	y = 0;
+void	flood_fill_rec(t_game *data, char **grid_copy, int y, int x)
+{
+	if (y < 0 || y >= data->map.height || x < 0 || x >= data->map.width)
+	{
+		free_grid(grid_copy);
+		exit_with_error("Map is not closed at the edges.", data);
+	}
+    if (grid_copy[y][x] == ' ')
+    {
+		free_grid(grid_copy);
+		exit_with_error("Map has a hole; floor is adjacent to a space.", data);
+	}
+	if (grid_copy[y][x] == '1' || grid_copy[y][x] == 'F')
+		return;
+	grid_copy[y][x] = 'F';
+	flood_fill_rec(data, grid_copy, y - 1, x);
+	flood_fill_rec(data, grid_copy, y + 1, x);
+	flood_fill_rec(data, grid_copy, y, x + 1);
+	flood_fill_rec(data, grid_copy, y, x - 1);
+}
 
-// 	while (data->map.grid[x])
-// 	{
-// 		x = 0;
-// 		while (data->map.grid[x][y])
-// 		{
-// 			c = data->map.grid[x][y];
-// 			if (!ft_strchr("NSEW01",c))
-// 			{
-// 				exit_with_error("the map has something not required",game);
-// 			}
-// 			if (!ft_strchr("NSEW",c))
-// 			{
-// 				data->map.map_player_x = x;
-// 				data->map.map_player_y = y;
-// 				data->map.player_count++;
-// 				data->map.spawn_side_face = c;
-// 				data->map.grid[x][y] = '0';
-// 			}
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// 	if (data->map.player_count != 1)
-// 		exit_with_error("ops there is more than one player",data);
-// }
+char	**duplicate_grid(char **grid, int height)
+{
+	char	**new_grid;
+	int		y;
+
+	new_grid = malloc(sizeof(char *) * (height + 1));
+	if (!new_grid)
+		return (NULL);
+	y = 0;
+	while (y < height)
+	{
+		new_grid[y] = ft_strdup(grid[y]);
+		if (!new_grid[y])
+		{
+			free_grid(new_grid); // Assuming you have a free_grid helper.
+			return (NULL);
+		}
+		y++;
+	}
+	new_grid[y] = NULL;
+	return (new_grid);
+}
+
+void    validate_walls_are_closed(t_game *data)
+{
+	char	**map_copy;
+
+	map_copy = duplicate_grid(data->map.grid, data->map.height);
+	if (!map_copy)
+		exit_with_error("Malloc failed for map copy.", data);
+	flood_fill_rec(data, map_copy,
+		(int)data->map.map_player_y, (int)data->map.map_player_x);
+	free_grid(map_copy);
+}
+
+int	get_max_width(char **grid)
+{
+	int	i;
+	int	max_width;
+
+	max_width = 0; 
+	i = 0;
+	while (grid[i])
+	{
+		if (ft_strlen(grid[i]) > max_width)
+			max_width = ft_strlen(grid[i]);	
+		i++;
+	}
+	return (max_width);
+}
+
+void	normalize_map_grid(t_game *data)
+{
+	char	**new_grid;
+	int		y;
+
+	data->map.width = get_max_width(data->map.grid);
+	if (data->map.width == 0)
+		exit_with_error("Map content is invalid or empty.", data);
+	new_grid = malloc(sizeof(char *) * (data->map.height + 1));
+	if (!new_grid)
+		exit_with_error("Malloc failed for new grid.", data);
+	y = 0;
+	while (y < data->map.height)
+	{
+		new_grid[y] = malloc(sizeof(char) * (data->map.width + 1));
+		if (!new_grid[y])
+			exit_with_error("Malloc failed for new grid row.", data);
+		ft_memset(new_grid[y], ' ', data->map.width);
+		ft_memcpy(new_grid[y], data->map.grid[y], ft_strlen(data->map.grid[y]));
+		new_grid[y][data->map.width] = '\0';
+		y++;
+	}
+	new_grid[y] = NULL;
+	free_grid(data->map.grid);
+	data->map.grid = new_grid;
+}
+
+
+void	validate_map_content(t_game *data)
+{
+	int		y;
+	int		x;
+	char	c;
+
+	y = 0;
+	data->map.player_count = 0;
+	while (data->map.grid[y])
+	{
+		x = 0;
+		while (data->map.grid[y][x])
+		{
+			c = data->map.grid[y][x];
+			if (!ft_strchr("01NSWE ", c))
+				exit_with_error("Invalid character in map.", data);
+			if (ft_strchr("NSWE", c))
+			{
+				data->map.player_count++;
+				data->map.map_player_x = (double)x + 0.5;
+				data->map.map_player_y = (double)y + 0.5;
+				data->map.spawn_side_face = c;
+				data->map.grid[y][x] = '0'; // Replace player with floor
+			}
+			x++;
+		}
+		y++;
+	}
+	if (data->map.player_count != 1)
+		exit_with_error("Map must have exactly one start position.", data);
+}
+
 
 void	create_map_grid(t_list **map_lines_head, t_game *data)
 {
@@ -130,69 +221,84 @@ void	create_map_grid(t_list **map_lines_head, t_game *data)
 	ft_lstclear(map_lines_head,free);
 }
 
-
-
-int main(int ac, char **av)
+static void	print_parsed_data(t_game *data)
 {
-	t_game  game;
-    // t_list  *all_lines;
-    // t_list  *id_lines;
-    // t_list  *map_lines;
-    
-    // if (ac != 2)
-    // {
-    //     ft_putstr_fd("Usage: ./cub3D <map.cub>\n", 2);
-    //     return (1);
-    // }
-    // // Zero out the entire game struct to initialize all pointers to NULL
-    // // and all flags/ints to 0. This is the safest way to start.
-    // ft_memset(&game, 0, sizeof(t_game));
-    
-    // if (validate_filename(av[1]) == 0)
-    //     exit_with_error("Invalid file extension. Must be .cub", &game);
+	int	i;
 
-    // // 1. Read the entire file into one list.
-    // all_lines = read_file_to_list(av[1]);
-    // if (!all_lines)
-    //     exit_with_error("File is empty or could not be read.", &game);
+	printf("\n--- PARSING COMPLETE ---\n");
+	printf("✅ Textures:\n");
+	printf("   NO: %s\n", data->asset_data.north_tex_path);
+	printf("   SO: %s\n", data->asset_data.south_tex_path);
+	printf("   WE: %s\n", data->asset_data.west_tex_path);
+	printf("   EA: %s\n", data->asset_data.east_tex_path);
+	printf("✅ Floor Color:   R=%d, G=%d, B=%d\n", data->asset_data.floor_rgb.red,
+		data->asset_data.floor_rgb.green, data->asset_data.floor_rgb.blue);
+	printf("✅ Ceiling Color: R=%d, G=%d, B=%d\n", data->asset_data.ceilllig_rgb.red,
+		data->asset_data.ceilllig_rgb.green, data->asset_data.ceilllig_rgb.blue);
+	printf("\n--- MAP DATA ---\n");
+	printf("Player Start: (x=%.1f, y=%.1f), Facing: %c\n", data->map.map_player_x,
+		data->map.map_player_y, data->map.spawn_side_face);
+	printf("Map Dimensions: Height=%d, Width=%d\n", data->map.height, data->map.width);
+	printf("--- NORMALIZED MAP GRID ---\n");
+	i = 0;
+	while (data->map.grid && data->map.grid[i])
+	{
+		printf("%s\n", data->map.grid[i]);
+		i++;
+	}
+	printf("--------------------------\n\n");
+}
 
-    // // 2. Separate the content into two new lists.
-    // id_lines = NULL;
-    // map_lines = NULL;
-    // separate_file_content(all_lines, &id_lines, &map_lines);
-    // printf("---PARSED 1---\n");
-    
-    // // 3. The original list is no longer needed. Free it immediately.
-    // ft_lstclear(&all_lines, free);
 
-    // // 4. Parse the identifier list, then free it.
-    // parse_identifiers(id_lines, &game);
-    // printf("---PARSED 2---\n");
-    // ft_lstclear(&id_lines, free);
-    
-    // // 5. Create the map grid from the map list.
-    // //    (The create_map_grid function will free the map_lines list itself).
-    // printf("---PARSED M3---\n");
-    // create_map_grid(&map_lines, &game);
+void	run_parser(char *filename, t_game *game)
+{
+	t_list	*all_lines;
+	t_list	*id_lines;
+	t_list	*map_lines;
 
-    // // 6. You can now proceed with map validation...
-    // int i = 0;
-    // while(game.map.grid[i])
-    // {
-    //     printf("==>%s\n", game.map.grid[i]);
-    //     i++;
-    // }
-    
-    // // Final cleanup at the end (will be part of your exit_game function)
-    // free_grid(game.map.grid);
-    // // You would also free your texture paths here.
+	if (validate_filename(filename) == 0)
+		exit_with_error("Invalid file extension. Must be .cub", game);
+	all_lines = read_file_to_list(filename);
+	if (!all_lines)
+		exit_with_error("File is empty or could not be read.", game);
+	id_lines = NULL;
+	map_lines = NULL;
+	separate_file_content(all_lines, &id_lines, &map_lines);
+	ft_lstclear(&all_lines, free);
+	parse_identifiers(id_lines, game);
+	ft_lstclear(&id_lines, free);
+	create_map_grid(&map_lines, game);
+	validate_map_content(game);
+	normalize_map_grid(game);
+	validate_walls_are_closed(game);
+}
 
-    // return (0);
+int	main(int ac, char **av)
+{
+	t_game	game;
+
+	// if (ac != 2)
+	// {
+	// 	ft_putstr_fd("Usage: ./cub3D <path_to_map.cub>\n", 2);
+	// 	return (1);
+	// }
+	// ft_memset(&game, 0, sizeof(t_game));
+	// run_parser(av[1], &game);
+	// print_parsed_data(&game);
+
+	// // ASSSUEM MEHDI WILL START Hre
+	// // launch_game(&game);
+	// free_grid(game.map.grid);
+	// free(game.asset_data.north_tex_path);
+	// free(game.asset_data.south_tex_path);
+	// free(game.asset_data.west_tex_path);
+	// free(game.asset_data.east_tex_path);
+	// return (0);
 	
 
-	//* mehdi Part______________________
-
-	char map[11][15] = {
+// 	//* mehdi Part______________________
+	
+char map[11][15] = {
     {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'},
     {'1','0','0','0','0','0','0','0','0','0','0','0','1','0','1'},
     {'1','0','0','1','0','1','0','0','0','0','0','0','1','0','1'},
@@ -206,13 +312,15 @@ int main(int ac, char **av)
     {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'}
 	};
 
-	int d = 0, j = 0;
+	int i = 0, j = 0;
 
 	game.map.height = 11;
-	game.map.width = 16;
+	game.map.width = 15;
+	game.map.map_player_x = 1.5;
+	game.map.map_player_y = 1.5;
 
-	game.player.x = 1.5 * TILE_SIZE; 
-	game.player.y = 1.5 * TILE_SIZE;
+	game.player.x = game.map.map_player_x * TILE_SIZE; 
+	game.player.y = game.map.map_player_y * TILE_SIZE;
 	game.player.rotation_angle = 0;
     game.player.turn_direction = 0;
     game.player.walk_direction = 0;
@@ -223,17 +331,16 @@ int main(int ac, char **av)
 	game.map.grid = malloc(sizeof(char *) * game.map.height);
     if (!game.map.grid)
 		return (EXIT_FAILURE);
+		
     for (int i = 0; i < game.map.height; i++) 
 	{
-        game.map.grid[i] = malloc(sizeof(char) * game.map.width);
+        game.map.grid[i] = malloc(sizeof(char) * (game.map.width + 1)); 
         if (!game.map.grid[i])
 			return (EXIT_FAILURE);
-        for (int j = 0; j < game.map.width - 1; j++){
-             game.map.grid[i][j] = map[i][j];
-        }
-         game.map.grid[i][game.map.width - 1] = '\0'; 
+        ft_memcpy(game.map.grid[i], map[i], game.map.width);
+        game.map.grid[i][game.map.width] = '\0';
     }
 	
 	intialize_mlx(&game);
-}
 
+}
--- cub3d/sources/rendering/ft_render.c
+++ cub3d/sources/rendering/ft_render.c
@@ -6,7 +6,7 @@
 /*   By: eel-garo <eel-garo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:42:51 by ymazini           #+#    #+#             */
-/*   Updated: 2025/07/15 17:00:03 by eel-garo         ###   ########.fr       */
+/*   Updated: 2025/07/16 12:14:44 by eel-garo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ void draw_square(t_game *game, int x, int y, int color)
 	int	minimap_tile_size;
 
 	minimap_tile_size = TILE_SIZE * MINIMAP_SCALE_FACTOR;
-	while (i < minimap_tile_size)
+	while (i < minimap_tile_size - 1)
 	{
 		j = 0;
-		while (j < minimap_tile_size)
+		while (j < minimap_tile_size - 1)
 		{
 			my_mlx_pixel_put(game, x + j, y + i, color);
 			j++;
@@ -53,7 +53,7 @@ void	draw_map(t_game *game)
 		while (x < game->map.width && game->map.grid[y][x])
 		{
 			if (game->map.grid[y][x] == '1')
-				color = 0x006272A4;
+				color = 0x37CACEFF;
 			else
 				color = 0xFFFFFFFF;
 			minimap_x = (x * TILE_SIZE) * MINIMAP_SCALE_FACTOR;
@@ -86,13 +86,89 @@ void draw_player(t_game *game)
 	}
 }
 
+/**
+  	X  = ? (Projection wall height)
+	1- Using Triangle Similarity:
+		A/B = D/C
+		D = X
+		A = actuial wall height = TILE_SIZE
+		B = distance to wall = ray distance (from raycasting)
+		C = distance from player to proj.plane = (WINDOW_WIDTH / 2) / tan(FOV / 2)
+		D = A/B * C
+   
+  
+*/
+
+void	render_3d(t_game *game, int wall_top_pixel, int wall_bottom_pixel, int i)
+{
+	int	y;
+
+	y = 0;
+	while (y < wall_top_pixel)
+		my_mlx_pixel_put(game, i, y++, 0x0087CEEB);
+	y = wall_top_pixel;
+	while (y < wall_bottom_pixel)
+		my_mlx_pixel_put(game, i, y++,0x00A9A9A9);
+	y = wall_bottom_pixel;
+	while (y < WINDOW_HEIGHT)
+		my_mlx_pixel_put(game, i, y++, 0xc29b3e);
+}
+
+void	render_3dproj(t_game *game)
+{
+	int	i;
+	float	dist_to_proj_plane;
+	float	projected_wall_height;
+	float	corrected_dist;
+
+	int		wall_top_pixel;
+	int		wall_bottom_pixel;
+
+	dist_to_proj_plane = (WINDOW_WIDTH / 2) / tan(FOV / 2);
+	i = 0;
+	while (i < WINDOW_WIDTH)
+	{
+		corrected_dist = game->rays[i].distance
+			* cos(game->rays[i].ray_angle - game->player.rotation_angle);
+				
+		projected_wall_height = (TILE_SIZE / corrected_dist) * dist_to_proj_plane;
+		
+		wall_top_pixel = (WINDOW_HEIGHT / 2) - (projected_wall_height / 2);
+		if (wall_top_pixel < 0)
+			wall_top_pixel = 0;
+		wall_bottom_pixel = (WINDOW_HEIGHT / 2) + (projected_wall_height / 2);
+		if (wall_bottom_pixel > WINDOW_HEIGHT)
+			wall_bottom_pixel = WINDOW_HEIGHT;
+		render_3d(game, wall_top_pixel, wall_bottom_pixel, i);
+		i++;
+	}	
+}
+
+
+void draw_minimap_rays(t_game *game)
+{
+    int i = 0;
+    while (i < WINDOW_WIDTH)
+    {
+        draw_line(
+            game,
+            game->player.x * MINIMAP_SCALE_FACTOR,
+            game->player.y * MINIMAP_SCALE_FACTOR,
+            game->rays[i].wall_hit_x * MINIMAP_SCALE_FACTOR,
+            game->rays[i].wall_hit_y * MINIMAP_SCALE_FACTOR
+        );
+        i++;
+    }
+}
 
 void	ft_render(t_game *game)
 {
 	mlx_clear_window(game->mlx, game->win);
 	update_player(game);
+	cast_rays(game);
+	render_3dproj(game);
 	draw_map(game);
 	draw_player(game);
-	cast_rays(game);
+	draw_minimap_rays(game);
 	mlx_put_image_to_window(game->mlx, game->win, game->img.img_ptr, 0, 0);
 }
